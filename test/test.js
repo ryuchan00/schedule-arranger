72acb9212aa3fdef607954e5460b28d80818b432
yamakawa00 <leokun0210@gmail.com>
2017-10-19 00:00:33 +0000

    update

--- test/test.js
+++ test/test.js
@@ -28,4 +28,11 @@ describe('/login', () => {
             .expect(/testuser/)
             .expect(200, done);
     });
+
+    it('/logout にアクセスした際に / にリダイレクトされる', (done) => {
+        request(app)
+            .get('/logout')
+            .expect('Location', '/')
+            .expect(302, done);
+    });
 });
